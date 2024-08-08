8232209e80d027a82a933eea3d4730b8a3b399d3
Eder Sparenberger <edsparen@gmail.com>
2024-08-08 17:21:48 -0300

    chore: Add student registration modal and form handling logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
 import { calculateAverage, evaluateAverage, addDisciplineRow, calculateAllAverages } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+
+  fetch('studentInsertModal.html')
+    .then((response) => response.text())
+    .then((html) => {
+      document.querySelector('#registerModal .modal-dialog').innerHTML = html;
+      fetchStudentData();
+      })
+    .catch((error) => {
+      console.error('Erro ao carregar o conteúdo do modal:', error);
+    });
+
   const allAverages = [];
   document.getElementById('btn_add_discipline_row').addEventListener('click', function () {
     const discipline = prompt('Qual a matéria deseja cadastrar?');
@@ -23,3 +34,26 @@ document.addEventListener('DOMContentLoaded', function () {
     evaluateAverage(average);
   });
 });
+
+
+function fetchStudentData() {
+  const form = document.getElementById('add_student_form');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Captura os valores do formulário
+    const studentNome = document.getElementById('inputName').value;
+    const studentAge = document.getElementById('inputAge').value;
+    const studentGrade = document.getElementById('inputGrade').value;
+    const studentSchool = document.getElementById('inputSchool').value;
+    const studentFavoriteDiscipline = document.getElementById('inputFavoriteDiscipline').value;
+
+    // Atualiza a seção "header-student-info"
+    document.getElementById('student_name').value = studentNome;
+    document.getElementById('student_age').value = studentAge;
+    document.getElementById('student_class').value = studentGrade;
+    document.getElementById('student_school').value = studentSchool;
+    document.getElementById('student_favorite_discipline').value = studentFavoriteDiscipline;
+    $('#registerModal').modal('hide'); // Fecha o modal
+  });
+}
